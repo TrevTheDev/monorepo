5588f980b2d647f1fb8167337b49d1aab2bcebb3
TrevTheDev <trevor.rabie@gmail.com>
2023-05-02 06:32:17 +0000

    npm fixes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
 /* eslint-disable */
-import { defineConfig, UserConfigExport } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 
-const config: UserConfigExport = {
-  test: {
-    include: ['./packages/validate/tests/**/*.ts', './packages/*/tests/**/*.js'],
-  },
-}
+// const config: UserConfigExport = {
+//   test: {
+//     include: ['./packages/validate/tests/**/*.ts', './packages/*/tests/**/*.js'],
+//   },
+// }
 
-if (process.env.DEBUG === 'jest') {
-  if (config.test) config.test.testTimeout = 5 * 60 * 1000
-}
+// if (process.env.DEBUG === 'jest') {
+//   if (config.test) config.test.testTimeout = 5 * 60 * 1000
+// }
 
 export default defineConfig({
   test: {
